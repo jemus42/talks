2aab1d42c22bcde28115f2d69a041c3991bbb3f3
Lukas Burk <lukas@quantenbrot.de>
2021-05-20 17:24:18 +0200

    WIP Emmy Lab talk

---
+++ emmylab/sddr.R
@@ -0,0 +1,8 @@
+if (!requireNamespace('deepregression')) {
+  remotes::install_github("davidruegamer/deepregression")
+}
+
+library(deepregression) # Will also install Python dependencies on first load
+library(palmerpenguins) # For simple example data
+
+
